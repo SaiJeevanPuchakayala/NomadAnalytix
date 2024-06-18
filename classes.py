75920cec6d549e2028d388013990bbeffdbd04ea
Allen Bijo <allenbijot@gmail.com>
2024-06-18 23:37:18 +0530

    modified to handle multiple files

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 import openai
 import google.generativeai as genai
 import os
+import re
+
 
 def run_request(question_to_ask, model_type):
     if model_type == "gpt-4" or model_type == "gpt-3.5-turbo" :
@@ -34,8 +36,9 @@ def run_request(question_to_ask, model_type):
         llm_response = model.generate_content(prompt)
 
     # rejig the response
-    llm_response = format_response(llm_response.text)
+    llm_response = find_code_blocks(format_response(llm_response.text))[0]
     return llm_response
+
 
 def format_response( res):
     # Remove the load_csv from the answer if it exists
@@ -57,6 +60,7 @@ def format_response( res):
         res = res_before + res_after
     return res
 
+
 def format_question(primer_desc,primer_code , question, model_type):
     # Fill in the model_specific_instructions variable
     instructions = ""
@@ -69,25 +73,40 @@ def format_question(primer_desc,primer_code , question, model_type):
     # Put the question at the end of the description primer within quotes, then add on the code primer.
     return  '"""\n' + primer_desc + question + '\n"""\n' + primer_code
 
-def get_primer(df_dataset,df_name):
+
+def get_primer(datasets):
+    primer_desc = "Label the x and y axes appropriately."
+    primer_desc = primer_desc + "\nAdd a title. Set the fig suptitle as empty."
+    primer_desc = primer_desc + "{}" # Space for additional instructions if needed
+    primer_desc = primer_desc + "\nUsing Python version 3.9.12, create a script using the dataframe df to graph the following: "
+    pimer_code = "import pandas as pd\nimport matplotlib.pyplot as plt\n"
+    pimer_code = pimer_code + "fig,ax = plt.subplots(1,1)\n"
+    pimer_code = pimer_code + "ax.spines['top'].set_visible(False)\nax.spines['right'].set_visible(False) \n"
+    for df_name in datasets:
+        pimer_code = pimer_code + f"{df_name} = datasets['{df_name}'].copy()\n"
+    return primer_desc, pimer_code
+
+
+def create_desc_primer(df_dataset):
     # Primer function to take a dataframe and its name
     # and the name of the columns
     # and any columns with less than 20 unique values it adds the values to the primer
     # and horizontal grid lines and labeling
-    primer_desc = "Use a dataframe called df from data_file.csv with columns '" \
+    primer_desc = f"This dataset has columns '" \
         + "','".join(str(x) for x in df_dataset.columns) + "'. "
     for i in df_dataset.columns:
         if len(df_dataset[i].drop_duplicates()) < 20 and df_dataset.dtypes[i]=="O":
             primer_desc = primer_desc + "\nThe column '" + i + "' has categorical values '" + \
                 "','".join(str(x) for x in df_dataset[i].drop_duplicates()) + "'. "
         elif df_dataset.dtypes[i]=="int64" or df_dataset.dtypes[i]=="float64":
-            primer_desc = primer_desc + "\nThe column '" + i + "' is type " + str(df_dataset.dtypes[i]) + " and contains numeric values. "   
-    primer_desc = primer_desc + "\nLabel the x and y axes appropriately."
-    primer_desc = primer_desc + "\nAdd a title. Set the fig suptitle as empty."
-    primer_desc = primer_desc + "{}" # Space for additional instructions if needed
-    primer_desc = primer_desc + "\nUsing Python version 3.9.12, create a script using the dataframe df to graph the following: "
-    pimer_code = "import pandas as pd\nimport matplotlib.pyplot as plt\n"
-    pimer_code = pimer_code + "fig,ax = plt.subplots(1,1,figsize=(10,4))\n"
-    pimer_code = pimer_code + "ax.spines['top'].set_visible(False)\nax.spines['right'].set_visible(False) \n"
-    pimer_code = pimer_code + "df=" + df_name + ".copy()\n"
-    return primer_desc,pimer_code
+            primer_desc = primer_desc + "\nThe column '" + i + "' is type " + str(df_dataset.dtypes[i]) + " and contains numeric values. "
+    return primer_desc
+
+def find_code_blocks(text):
+    # Define a regular expression pattern to match ``` followed by anything
+    # (non-greedy matching with .*?) and then again ```
+    pattern = r"```python(.*?)```"
+
+    # Use re.findall to find all occurrences of the pattern
+    code_blocks = re.findall(pattern, text, re.DOTALL)  # re.DOTALL allows matching across lines
+    return code_blocks
--- runner.py
+++ runner.py
@@ -1,48 +1,61 @@
-#################################################################################
-# Chat2VIS
-# https://chat2vis.streamlit.app/
-# Paula Maddigan
-#################################################################################
-
 import pandas as pd
-import openai
-from classes import get_primer, format_question, run_request
+from classes import get_primer, format_question, run_request, create_desc_primer
 import warnings
 from dotenv import load_dotenv
+import os
 
 warnings.filterwarnings("ignore")
 load_dotenv()
 
-available_models = {"Gemini":"gemini-1.5-flash", "ChatGPT-4": "gpt-4","ChatGPT-3.5": "gpt-3.5-turbo-16k"}
+available_models = {"Gemini":"gemini-1.5-flash", "ChatGPT-4o": "gpt-4o", "ChatGPT-3.5": "gpt-3.5-turbo-16k"}
 # gpt-3.5-turbo-16k, gpt-4o-2024-05-13, gpt-4o
 
-datasets = {}
-datasets["Movies"] = pd.read_csv("movies.csv")
 
-selected_models = ["Gemini", "ChatGPT-3.5"]
-model_count = 1
+def create_filename_dict(folder_path):
+	# Create an empty dictionary to store the filenames
+	filename_dict = {}
 
-# Text area for query
-question = "what is the 10 highest r rated movie?"
-chosen_dataset = "Movies"
+	# Iterate over all files in the specified folder
+	for filename in os.listdir(folder_path):
+		# Check if the current item is a file (and not a directory)
+		if os.path.isfile(os.path.join(folder_path, filename)):
+			# Split the filename into name and extension
+			name, extension = os.path.splitext(filename)
+			# Add the name and full filename to the dictionary
+			filename_dict[name] = pd.read_csv(os.path.join(folder_path, filename))
 
-# Execute chatbot query
-if model_count > 0:
-	# Get the primer for this dataset
-	primer1, primer2 = get_primer(datasets[chosen_dataset], 'datasets["' + chosen_dataset + '"]')
+	return filename_dict
+
+
+def main(datasets, selected_model, question):
+	# Execute chatbot query
+	primer0 = f'There are {len(datasets)} datasets available.'
+	for i, chosen_dataset in enumerate(datasets):
+		primer0 += f'\nDataset {i + 1} is {chosen_dataset}.\n'
+		primer0 += create_desc_primer(datasets[chosen_dataset]) + '\n'
+
+	primer1, primer2 = get_primer(datasets)
+	primer1 = primer0 + primer1
 	# Create model, run the request and print the results
 	# Format the question
-	question_to_ask = format_question(primer1, primer2, question, selected_models[0])
+	question_to_ask = format_question(primer1, primer2, question, selected_model)
+
 	# Run the question
-	answer = ""
-	answer = run_request(question_to_ask, available_models[selected_models[1]])
+	answer = run_request(question_to_ask, available_models[selected_model])
 	# the answer is the completed Python script so add to the beginning of the script to it.
-	# answer = primer2 + answer
-	# print("Model: " + model_type)
-	print(answer)
 
 	# print(question_to_ask)
+	print(answer)
+	exec(answer)
 
 
+if __name__ == "__main__":
+	datasets = create_filename_dict("files")
 
+	# selected_model = "Gemini"
+	selected_model = "ChatGPT-3.5"
 
+	# Text area for query
+	question = "Which are the Top 2 teams with the most wins achieved by chasing targets over the past 3 years?"
+
+	main(datasets, selected_model, question)
